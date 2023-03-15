377c40b359c7e5418faf13e465f0247622621f10
Noah Lett <noahblett96@gmail.com>
2023-03-15 23:40:03 +0000

    Created users table of Postgresql database

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,12 @@
 set client_min_messages to warning;
 
 create schema "public";
+
+CREATE TABLE "public"."users" (
+  "userId" SERIAL PRIMARY KEY,
+  "firstName" TEXT NOT NULL,
+  "lastName" TEXT NOT NULL,
+  "email" TEXT NOT NULL UNIQUE,
+  "hashedPassword" TEXT NOT NULL,
+  "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
