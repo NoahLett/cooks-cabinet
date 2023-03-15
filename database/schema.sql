2aafc3eb912540de897916a36493b2a1ed2647f8
Noah Lett <noahblett96@gmail.com>
2023-03-15 23:46:31 +0000

    Created recipes table of Postgresql database

--- database/schema.sql
+++ database/schema.sql
@@ -10,3 +10,17 @@ CREATE TABLE "public"."users" (
   "hashedPassword" TEXT NOT NULL,
   "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE "public"."recipes" (
+  "recipeId" SERIAL PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "description" TEXT NOT NULL,
+  "photoUrl" TEXT NOT NULL,
+  "steps" TEXT[] NOT NULL,
+  "ingredients" TEXT[] NOT NULL,
+  "createdAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "updatedAt" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "deleted" BOOLEAN DEFAULT FALSE,
+  "userId" INTEGER NOT NULL,
+  CONSTRAINT "recipes_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId")
+);
