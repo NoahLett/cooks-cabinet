d38ac715b436ec4404bd890099983ab231c24940
Noah Lett <noahblett96@gmail.com>
2023-03-16 17:44:02 +0000

    Added react-icons and react router dependencies

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import Home from './pages/home';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <>
+        <Navbar/>
+        <Home />
+      </>
+    );
   }
 }
