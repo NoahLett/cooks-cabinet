0ecf98ea5c79fe51ce53398e6dac6b9d4c53eaa6
Noah Lett <noahblett96@gmail.com>
2023-03-16 18:05:20 +0000

    added methods to navbar component

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 export default function Navbar() {
+
+  const [click, setClick] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const handleHover = () => setIsHovered(true);
+  const handleMouseOut = () => setIsHovered(false);
+
   return (
-    <h1>Hello, World!</h1>
+    <div />
   );
 }
