e67c5a669b141c5075874bb013ab52bfe8195bd4
Noah Lett <noahblett96@gmail.com>
2023-03-17 22:37:49 +0000

    Added basic slides array of objects with skeleton data

---
+++ client/lib/carousel-slides.js
@@ -0,0 +1,20 @@
+const slides = [
+  {
+    title: 'Slide 1',
+    imageUrl: 'https://example.com/slide1.jpg'
+  },
+  {
+    title: 'Slide 2',
+    imageUrl: 'https://example.com/slide2.jpg'
+  },
+  {
+    title: 'Slide 3',
+    imageUrl: 'https://example.com/slide3.jpg'
+  },
+  {
+    title: 'Slide 4',
+    imageUrl: 'https://example.com/slide4.jpg'
+  }
+];
+
+export default slides;
