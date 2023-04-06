4a6de94299897a255f278c571072ac92f50a85af
Noah Lett <noahblett96@gmail.com>
2023-04-06 19:21:20 +0000

    Added initial CSS styling to recipe cards

--- client/components/RecipeFeed.jsx
+++ client/components/RecipeFeed.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './RecipeFeed.css';
 
 function RecipeFeed() {
   const [recipes, setRecipes] = useState([]);
@@ -11,12 +12,11 @@ function RecipeFeed() {
   }, []);
 
   return (
-    <div className="container">
-      <h1>Catalog</h1>
-      <hr />
-      <div className="row">
+    <div className="card-container">
+      <h1 className='recipes-title'>Recipes</h1>
+      <div className="cards">
         {recipes.map(recipe => (
-          <div key={recipe.recipeId} className="col-12 col-md-6 col-lg-4">
+          <div key={recipe.recipeId}>
             <Recipe recipe={recipe} />
           </div>
         ))}
@@ -28,16 +28,18 @@ function RecipeFeed() {
 function Recipe(props) {
   const { recipeId, name, photoUrl, description } = props.recipe;
   return (
-    <Link
+    <div className='card'>
+      <Link
       to={`/recipes?recipeId=${recipeId}`}
       className='recipe-link'
     >
-      <img src={photoUrl} className='card-img-top' alt={name} />
-      <div className='card-body'>
-        <h5 className='card-title'>{name}</h5>
-        <p className='card-text'>{description}</p>
-      </div>
-    </Link>
+        <img src={photoUrl} className='card-img-top' alt={name} />
+        <div className='card-body'>
+          <h5 className='card-title'>{name}</h5>
+          <p className='card-text'>{description}</p>
+        </div>
+      </Link>
+    </div>
   );
 }
 
--- database/data.sql
+++ database/data.sql
@@ -2,6 +2,6 @@ INSERT INTO "users" ("username", "hashedPassword")
 VALUES ('johndoe', 'Mysecretpassword1!');
 
 INSERT INTO "recipes" ("name", "description", "photoUrl", "steps", "ingredients", "userId")
-VALUES ('Chocolate Cake', 'A delicious chocolate cake recipe', 'https://example.com/chocolate-cake.jpg',
+VALUES ('Chocolate Cake', 'A delicious chocolate cake recipe', 'https://www.hersheyland.com/content/dam/hersheyland/en-us/recipes/recipe-images/2_Hersheys_Perfectly_Chocolate_Cake_11-18.jpeg',
         '{"Preheat the oven to 350F","Mix dry ingredients","Mix wet ingredients","Combine dry and wet ingredients","Bake for 30 minutes"}',
         '{"Flour","Sugar","Cocoa powder","Eggs","Milk","Oil","Vanilla extract","Baking powder","Salt"}', 1);
